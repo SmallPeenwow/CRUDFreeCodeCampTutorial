71e4eb4e44cc51d4a6ded75be7fceed9edb18d1a
SmallPeenwow <bowdencamryn@gmail.com>
2023-08-23 21:46:41 +0200

    Add project files.

---
+++ CRUDFreeCodeCampTutorial/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUDFreeCodeCampTutorial.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
