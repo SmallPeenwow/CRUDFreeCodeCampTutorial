4bb1494c551efcff9f1e4a25c7f6d1fa3a7d5314
SmallPeenwow <bowdencamryn@gmail.com>
2023-08-26 14:24:49 +0200

    ApplicationDbContext Done

---
+++ CRUDFreeCodeCampTutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CRUDFreeCodeCampTutorial.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDFreeCodeCampTutorial.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+    }
+}
