3175693d7d7c9f9ce72543765b2ac20ad512625c
SmallPeenwow <bowdencamryn@gmail.com>
2023-08-26 20:16:39 +0200

    Program.cs Database Settings Done

--- CRUDFreeCodeCampTutorial/Data/ApplicationDbContext.cs
+++ CRUDFreeCodeCampTutorial/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace CRUDFreeCodeCampTutorial.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
         public DbSet<Category> Category { get; set; }
     }
 }
