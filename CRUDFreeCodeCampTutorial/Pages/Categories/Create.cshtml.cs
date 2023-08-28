177ca16253df1850f0091e13797f1cfed0844fc3
SmallPeenwow <bowdencamryn@gmail.com>
2023-08-28 13:00:19 +0200

    Create Category And Bind Property Done

--- CRUDFreeCodeCampTutorial/Pages/Categories/Create.cshtml.cs
+++ CRUDFreeCodeCampTutorial/Pages/Categories/Create.cshtml.cs
@@ -1,15 +1,33 @@
+using CRUDFreeCodeCampTutorial.Data;
 using CRUDFreeCodeCampTutorial.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CRUDFreeCodeCampTutorial.Pages.Categories
 {
+    // [BindProperties] Top Level binding
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
         public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
